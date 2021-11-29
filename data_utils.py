d0261da13e9e48896b55dca66f475f92528e3998
kevin1zc <kevinliuzc01@gmail.com>
2021-11-29 00:52:01 -0800

    Convert data to PyTorch tensors and implement training pipeline

---
+++ data_utils.py
@@ -0,0 +1,72 @@
+from collections import defaultdict
+from torch.utils.data import Dataset
+import json
+
+
+class DECODE(Dataset):
+    """Face Landmarks dataset."""
+
+    def __init__(self, data_file, unstructured=False):
+        with open(data_file, 'r') as f:
+            data = f.read().splitlines()
+            self.data = [json.loads(line) for line in data]
+        num_len = set()
+        for d in self.data:
+            num_len.add(len(d["aggregated_contradiction_indices"]))
+        self.unstructured = unstructured
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return self.parse_json(self.data[idx], self.unstructured)
+
+    # TODO:
+    def parse_json(self, instance, unstructured):
+        label = instance['aggregated_contradiction_indices']
+        is_contradiction = 1 if instance["is_contradiction"] else 0
+        all_utterances = []
+        # stores utterances as dictionary of lists, keyed by agent_id
+        speaker_utterances = defaultdict()
+        last_speaker = -1
+        for turn in instance['turns']:
+            all_utterances.append(turn['text'])
+            last_speaker = turn['agent_id']
+            try:
+                speaker_utterances[turn['agent_id']].append(turn['text'])
+            except KeyError:
+                speaker_utterances[turn['agent_id']] = [turn['text']]
+
+        if unstructured:
+            return all_utterances, is_contradiction
+        else:
+            utterance_str = []
+            for utt in speaker_utterances[last_speaker]:
+                utterance_str.append(utt)
+            return utterance_str, is_contradiction
+
+    # TODO: This one follows the data preparation described in paper. Need to think of how to make it mini-batches.
+    # TODO: Problem: one dialog instance have variable numbers of (prev, last) utterance pairs.
+    # def parse_json(self, instance, unstructured):
+    #     contradiction_indices = instance['aggregated_contradiction_indices']
+    #     is_contradiction = 1 if instance["is_contradiction"] else 0
+    #     all_utterances = []
+    #     # stores utterances as dictionary of lists, keyed by agent_id
+    #     speaker_utterances = defaultdict()
+    #     last_speaker = -1
+    #     for turn in instance['turns']:
+    #         all_utterances.append((turn['agent_id'], turn['text']))
+    #         last_speaker = turn['agent_id']
+    #         try:
+    #             speaker_utterances[turn['agent_id']].append((turn['turn_id'], turn['text']))
+    #         except KeyError:
+    #             speaker_utterances[turn['agent_id']] = [(turn['turn_id'], turn['text'])]
+    #
+    #     if unstructured:
+    #         return all_utterances, is_contradiction
+    #     else:
+    #         utterance_pairs = []
+    #         utterances_one_side = speaker_utterances[last_speaker]
+    #         for i in range(len(utterances_one_side) - 1):
+    #             utterance_pairs.append((utterances_one_side[i], utterances_one_side[-1]))
+    #         return utterance_pairs, is_contradiction, contradiction_indices
--- main.py
+++ main.py
@@ -1,73 +1,127 @@
 import torch
+from torch import nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from data_utils import DECODE
+from fairseq.data.data_utils import collate_tokens
+from functools import partial
+from tqdm import tqdm
 import numpy as np
-import json
+import pickle
 
-from collections import defaultdict
 
-torch.hub._validate_not_a_forked_repo = lambda a, b, c: True
-roberta = torch.hub.load('pytorch/fairseq', 'roberta.base')
-roberta.eval()  # disable dropout (or leave in train mode to finetune)
+def collate_batch(batch, roberta, unstructured=False):
+    if unstructured:
+        pass
+    else:
+        utterances_batch, labels_batch = [], []
+        for _utterances_pairs, _labels in batch:
+            utterances_batch.append(roberta.encode(*_utterances_pairs))
+            labels_batch.append(_labels)
 
-tokens = roberta.encode('Hello world!')
-assert tokens.tolist() == [0, 31414, 232, 328, 2]
-assert roberta.decode(tokens) == 'Hello world!'
+        utterances_batch = collate_tokens(utterances_batch, pad_idx=1)
+        labels_batch = torch.tensor(labels_batch, dtype=torch.int64)
 
-# Extract the last layer's features
-last_layer_features = roberta.extract_features(tokens)
-print(last_layer_features)
+        return utterances_batch, labels_batch
 
-# TODO optimize for CUDA on CARC?
-def batch_generator(data, batch_size, unstructured=True, isLabelled=True):
-	n = len(data)
-	# creates random permuation of indices [0,...,n-1]
-	permutedIndices = np.random.permutation(range(n))
 
-	for k in range(0, n, batch_size):
-		batch = [parse_json(data[i], unstructured) for i in permutedIndices[k:k+batch_size]]
-		# batch = torch.tensor(np.array(batch))
+# TODO: Process data according to the paper. Not finished yet.
+# def collate_batch(batch, roberta, unstructured=False):
+#     if unstructured:
+#         pass
+#     else:
+#         utterances_batch, labels_batch = [], []
+#         for utterances_pairs, is_contradiction, contradiction_indices in batch:
+#             utterances_instance = []
+#             for prev, last in utterances_pairs:
+#                 prev_idx, prev_utterance = prev
+#                 last_idx, past_utterance = last
+#                 utterances_instance.append(roberta.encode(prev_utterance, past_utterance))
+#             utterances_instance = collate_tokens(utterances_instance, pad_idx=1)
+#             utterances_batch.append(utterances_instance)
+#             labels_batch.append(is_contradiction)
+#
+#         utterances_batch = collate_tokens(utterances_batch, pad_idx=1)
+#         labels_batch = torch.tensor(labels_batch, dtype=torch.int64)
+#
+#         return utterances_batch, labels_batch
 
-		yield batch
 
-		# TODO integrate support for both labelled and unlabelled datasets
-		# batch_X = torch.tensor(batch_X) if tensorize else batch_X
-		# if isLabelled:
-		# 	batch_labels = np.array([self.one_hot_labels[i] for i in permutedIndices[k:k+batch_size]])
-		# 	batch_labels = torch.tensor(batch_labels) if tensorize else batch_labels
+def train(model, data_loader, loss_fn, optimizer):
+    losses = []
+    model.train()
+    for utterances, labels in tqdm(data_loader):
+        utterances = utterances.cuda()
+        labels = labels.cuda()
+        model.zero_grad()
+        labels_pred = roberta.predict('contradiction_detect', utterances)
+        loss = loss_fn(labels_pred, labels)
+        loss.backward()
+        optimizer.step()
+        losses.append(loss.item())
+    return np.mean(losses)
 
-		# 	yield batch_X, batch_labels			
-		# else:
-		# 	yield batch_X
 
-# parses one instance of json-structured data
-# if unstructured, it will embed and concatenate all utterances
-# else, it will just embed the utterances from the final speaker
-def parse_json(instance, unstructured):
-	label = instance['aggregated_contradiction_indices']
-	all_utterances = ''
-	# stores utterances as dictionary of lists, keyed by agent_id
-	speaker_utterances = defaultdict()
-	last_speaker = -1
-	for turn in instance['turns']:
-		all_utterances += turn['text']
-		last_speaker = turn['agent_id']
-		try:
-			speaker_utterances[turn['agent_id']].append(turn['text'])
-		except KeyError:
-			speaker_utterances[turn['agent_id']] = [turn['text']]
+def validate(model, data_loader, loss_fn):
+    losses = []
+    correct = 0
+    with torch.no_grad():
+        model.eval()
+        for utterances, labels in tqdm(data_loader):
+            utterances = utterances.cuda()
+            labels = labels.cuda()
+            labels_pred = roberta.predict('contradiction_detect', utterances)
+            correct += torch.sum(torch.argmax(labels_pred, dim=1) == labels).item()
+            loss = loss_fn(labels_pred, labels)
+            losses.append(loss.item())
+    return np.mean(losses), correct / len(data_loader.dataset)
 
-	if unstructured:
-		return all_utterances, label
-	else:
-		utterance_str = ''
-		for utt in speaker_utterances[last_speaker]:
-			utterance_str += utt
-		return utterance_str, label
 
-# loads jsonl file into memory (hopefully this won't crash on CARC :P)
-def load_jsonl(fname):
-	data = None
-	with open(fname, 'r') as f:
-		data = f.read().splitlines()
-		data = [json.loads(line) for line in data]
+if __name__ == "__main__":
+    EPOCHS = 5
+    BATCH_SIZE = 8
 
-	return data
+    torch.hub._validate_not_a_forked_repo = lambda a, b, c: True
+    roberta = torch.hub.load('pytorch/fairseq', 'roberta.base')
+    roberta.register_classification_head('contradiction_detect', num_classes=2)
+
+    unstructured = False
+    train_data = DECODE("decode_v0.1/train.jsonl", unstructured)
+    val_data = DECODE("decode_v0.1/dev.jsonl", unstructured)
+    test_data = DECODE("decode_v0.1/test.jsonl", unstructured)
+
+    train_dataloader = DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True,
+                                  collate_fn=partial(collate_batch, roberta=roberta, unstructured=unstructured))
+    val_dataloader = DataLoader(val_data, batch_size=BATCH_SIZE, shuffle=True,
+                                collate_fn=partial(collate_batch, roberta=roberta, unstructured=unstructured))
+    test_dataloader = DataLoader(test_data, batch_size=BATCH_SIZE, shuffle=True,
+                                 collate_fn=partial(collate_batch, roberta=roberta, unstructured=unstructured))
+
+    roberta.cuda()
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(roberta.parameters(), lr=0.0001, weight_decay=1e-8)
+
+    train_losses = []
+    val_losses = []
+    val_accs = []
+
+    for epoch in range(EPOCHS):
+        print(f"Epoch: {epoch + 1}")
+        train_loss = train(roberta, train_dataloader, loss_fn, optimizer)
+        print(f"    Train loss: {train_loss}")
+        train_losses.append(train_loss)
+
+        val_loss, val_acc = validate(roberta, val_dataloader, loss_fn)
+        print(f"    Validation loss: {val_loss}")
+        print(f"    Validation accuracy: {val_acc}")
+        val_losses.append(val_loss)
+        val_accs.append(val_acc)
+
+    test_loss, test_acc = validate(roberta, test_dataloader, loss_fn)
+    print("*" * 32)
+    print(f"Test loss: {test_loss}")
+    print(f"Test accuracy: {test_acc}")
+
+    results = (train_losses, val_losses, val_accs, test_loss, test_acc)
+    with open('results.pickle', 'wb') as f:
+        pickle.dump(results, f)
